9994b74cb83c7701b1b697d6b4946ec6b5781753
liushiyizu <1623685118@qq.com>
2018-12-21 21:05:46 +0800

    http://liushiyizu.top

--- client/webApp/mobile/ssr/config.js
+++ client/webApp/mobile/ssr/config.js
@@ -1,6 +1,6 @@
 const Vue = require("vue");
-var ctx = "http://10.0.0.126";
-var api = "http://10.0.0.126/api";
+var ctx = "http://www.liushiyizu.top";
+var api = "http://www.liushiyizu.top/api";
 Vue.prototype.$ctx = ctx;
 Vue.prototype.$api = api;
 module.exports = {};
